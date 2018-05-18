ed3e603c75f014239d1d0f419b1d1fc6d546a809
Andrej Benz <hello@andrejbenz.com>
2018-05-18 14:38:41 +0200

    added post route for index to check cache invalidation

--- handlers/index.go
+++ handlers/index.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/abenz1267/rufus"
@@ -18,6 +19,7 @@ type Index struct {
 // GetRoutes registers routes for Index handler
 func (h Index) GetRoutes(r *chi.Mux) {
 	r.Get("/", h.get)
+	r.Post("/", h.post)
 }
 
 func (h Index) get(w http.ResponseWriter, r *http.Request) {
@@ -29,4 +31,10 @@ func (h Index) get(w http.ResponseWriter, r *http.Request) {
 	h.App.Response = resp
 
 	h.App.Render(w, r)
+}
+
+func (h Index) post(w http.ResponseWriter, r *http.Request) {
+	// do something
+	deleted := h.App.Cache.Invalidate(r.RequestURI)
+	fmt.Println("Cache deleted:", deleted)
 }
